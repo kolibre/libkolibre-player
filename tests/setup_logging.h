2050a485bc07f29d8c079b1e81a4f0e353c7a9b6
Johan Abbors <johan@pratsam.com>
2013-03-21 12:08:20 +0200

    Enable file logging for tests

--- tests/setup_logging.h
+++ tests/setup_logging.h
@@ -33,16 +33,21 @@ void setup_logging()
     log4cxx::LayoutPtr layoutPtr(new log4cxx::PatternLayout("%d{MMM dd yyyy HH:mm:ss} %c:%L [%-5t] [%p] : %m%n"));
 
     // create FileAppender
-    //LOG4CXX_DECODE_CHAR(fileName, "player.log");
-    //log4cxx::FileAppenderPtr file(new log4cxx::FileAppender(layoutPtr, fileName));
+    LOG4CXX_DECODE_CHAR(fileName, "player.log");
+    log4cxx::FileAppenderPtr file(new log4cxx::FileAppender(layoutPtr, fileName));
 
     // create ConsoleAppender
     log4cxx::ConsoleAppenderPtr console(new log4cxx::ConsoleAppender(layoutPtr));
 
     // set log level
-    logger->setLevel(log4cxx::Level::getInfo());
+    //logger->setLevel(log4cxx::Level::getTrace());
+    logger->setLevel(log4cxx::Level::getDebug());
+    //logger->setLevel(log4cxx::Level::getInfo());
+    //logger->setLevel(log4cxx::Level::getWarn());
+    //logger->setLevel(log4cxx::Level::getError());
+    //logger->setLevel(log4cxx::Level::getFatal());
 
     // add appenders and other will inherit the settings
-    //logger->addAppender(file);
+    logger->addAppender(file);
     logger->addAppender(console);
 }
